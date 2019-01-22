96a349edea5ae445e797d3eb3fe69fb836a52ca4
Shane Drafahl <drafahls@gmail.com>
2019-01-22 16:43:44 -0600

    Finished twoPrimes

--- primes.go
+++ primes.go
@@ -3,7 +3,19 @@ package rsa
 import "math"
 
 func twoPrimes(index int) (int, int) {
-
+	prime1, prime2 := 0, 0
+	for i := 2; i < index; i++ {
+		if isPrime(index) {
+			if prime1 == 0 {
+				prime1 = index
+			} else {
+				if prime2 == 0 {
+					prime2 = index
+				}
+			}
+		}
+	}
+	return prime1, prime2
 }
 
 func isPrime(numberInQuestion int) bool {
