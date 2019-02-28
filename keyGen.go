31ebb40e1d883b7857abca9b4e7340dbba50dc3a
shane <sdrafahl@iastate.edu>
2019-02-27 23:46:08 -0600

    Finished gen function

--- keyGen.go
+++ keyGen.go
@@ -2,11 +2,13 @@ package rsa
 
 import "math"
 
-func gen() {
+func gen() (int, int) {
 	p, q := twoPrimes(100)
 	n := p * q
 	totient := (p - 1) * (q - 1)
 	e := find_e(totient)
+	d := computeD(totient, e)
+	return (n, d)
 }
 
 func find_e(totient int) int {
@@ -19,7 +21,38 @@ func find_e(totient int) int {
 	return e
 }
 
-func euclid(totient int, e int) int {
+func getFactor(factor int, polymer Polynomial, polymers []Polynomial) int {
+	var factorValue1 int = 0
+	if polymer.answer == factor {
+		factorValue1 = 1
+	} else {
+		var polymer Polynomial = searchPolynomials(factor, polymers)
+		if polymer != nil {
+			factorValue1 = getFactor(factor, polymer, polymers)
+		}
+	}
+	var factorValue2 int = 0
+	if polymer.factor1 == factor {
+		factorValue2 = polymer.factor2 * -1
+	} else {
+		var polymer Polynomial = searchPolynomials(factor, polymers)
+		if polymer != nil {
+			factorValue2 = getFactor(factor, polymer, polymers) * polymer.factor2 * -1
+		}
+	}
+	return factorValue1 + factorValue2
+}
+
+func searchPolynomials(remainder int, polynomials []Polynomial) Polynomial {
+	for i, s := range polynomials {
+		if s.remainder == remainder {
+			return s
+		}
+	}
+	return nil
+}
+
+func computeD(totient int, e int) int {
 	var polys []Polynomial
 	answer := totient
 	factor1 := e
@@ -35,9 +68,9 @@ func euclid(totient int, e int) int {
 		factor1 = remainder
 		remainder = int(math.Mod(float64(answer), float64(factor1)))
 	}
-	for i := 0; i < len(polys); i++ {
-
-	}
+	var polyNomial Polynomial = (polys[i:i+1])[0]
+	var d int = getFactor(polyNomial, polys)
+	return d
 }
 
 type Polynomial struct {
